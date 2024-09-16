e38a48315a99249c9288d701b4aea0f222410c97
Duman Haydar <haydar@duman.io>
2024-09-16 14:47:17 +0200

    feat: add remove multiselect and create to tag

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,5 +2,11 @@ require('./bootstrap');
 import TomSelect from 'tom-select';
 import 'tom-select/dist/css/tom-select.css';
 
-new TomSelect("#tags");
-new TomSelect("#authors_ids");
+new TomSelect("#tags", {
+    plugins: ['no_backspace_delete','remove_button'],
+    createOnBlur: true,
+    create: true
+});
+new TomSelect("#authors_ids", {
+    plugins: ['no_backspace_delete','remove_button']
+});
