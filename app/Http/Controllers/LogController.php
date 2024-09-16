e9af7888fd86636ff7240c53c3d4a32ae6e36c45
Duman Haydar <haydar@duman.io>
2024-09-16 13:50:00 +0200

    feat: update controllers

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -4,26 +4,43 @@ namespace App\Http\Controllers;
 
 use App\Models\Media;
 use App\Models\Post;
+use App\Models\PostAuthor;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class LogController extends BaseController
 {
     public function index(Request $request) {
         $posts = Post::all();
 
+        $tags = null;
+        $users = null;
+        $currentUserId = null;
+        if(Auth::check()) {
+            foreach($posts as $post) {
+                $post->authors = $post->postAuthors->map(function($author) { return $this->getProfileById($author->user_id)->username; })->toArray();
+            }
 
-        return view('log/index', ['posts' => $posts]);
+            $tags = Tag::all();
+            $users = $this->getUsers();
+            $currentUserId = $this->getProfile()->id;
+        }
+
+        return view('log/index', ['posts' => $posts, 'users' => $users, 'tags' => $tags, 'current_user_id' => $currentUserId]);
     }
 
-    public function create(Request $request): void
+    public function create(Request $request): \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
     {
 
         $validated = $request->validate([
             'title' => 'required|string|max:255',
-            'author_id' => 'required|uuid',
+            'authors_ids' => 'array',
+            'authors_ids.*' => 'uuid',
             'tags' => 'nullable|array',
             'tags.*' => 'uuid|exists:tags,id',
             'media_files' => 'nullable|array',
@@ -31,12 +48,22 @@ class LogController extends BaseController
         ]);
 
         DB::beginTransaction();
+
         try {
-            $post = Post::create([
+            $post = new Post([
                 'title' => $validated['title'],
-                'submitter_id' => Auth::id(),
-                'author_id' => $validated['author_id'],
+                'submitter_id' => $this->getProfile()->id,
             ]);
+
+            $post->save();
+
+            foreach($validated['authors_ids'] as $authorId) {
+                $postAuthor = new PostAuthor([
+                    'user_id' => $authorId,
+                    'post_id' => $post->id
+                ]);
+                $postAuthor->save();
+            }
 
             if (!empty($validated['tags'])) {
                 $post->tags()->attach($validated['tags']);
@@ -44,46 +71,95 @@ class LogController extends BaseController
 
             if ($request->hasFile('media_files')) {
                 foreach ($request->file('media_files') as $file) {
-                    $file->store('media', 'public');
-
                     $media = new Media([
-                        'extension' => $file->getExtension(),
-                        'size' => $file->getSize(),
-                        'post_id' => $post->id,
+                        'extension' => $file->getClientOriginalExtension(),
+                        'size' => $file->getSize()
                     ]);
 
+                    $media->post()->associate($post);
                     $media->save();
+
+                    $file->storeAs('media', $media->id.'.'.$file->getClientOriginalExtension(), 'public');
                 }
             }
 
             DB::commit();
         } catch (\Exception $e) {
             DB::rollBack();
-            abort(422);
+            abort(422, $e->getMessage());
         }
 
-        redirect("index");
+        return redirect(route("index"));
     }
 
     public function show(Request $request, string $id) {
         $post = Post::where('id', $id)->first();
         if($post) {
-            $tags = $post->tags;
-            $medias = $post->medias;
-
+            $post->author = $this->getProfileById($post->author_id);
             return view('log/show', ['post' => $post]);
         }
 
         abort(404);
     }
 
-    public function delete(Request $request, string $id): void
+    public function delete(Request $request, string $id): \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
     {
         $post = Post::where('id', $id)->first();
         if($post->author_id == Auth::id() || $post->submitter_id == Auth::id()) {
             $post->delete();
         }
 
-        redirect("index");
+        return redirect(route("index"));
+    }
+
+    protected function getToken() {
+        $response = Http::asForm()->post(env('KEYCLOAK_BASE_URL', '').'/realms/master/protocol/openid-connect/token', [
+            "grant_type" => "client_credentials",
+            "client_id" => "admin-cli",
+            "client_secret" => env('KEYCLOAK_ADMINCLI_SECRET', '')
+        ]);
+        return $response->ok() && isset($response->json()['access_token']) ? $response->json()['access_token'] : null;
+    }
+
+    protected function getProfile(): object|bool
+    {
+        $token = $this->getToken();
+        if($token) {
+            $response = Http::withHeaders([
+                'Authorization' => 'Bearer '.$token
+            ])->get(env('KEYCLOAK_BASE_URL', '').'/admin/realms/'.env('KEYCLOAK_REALM', 'master').'/users', [
+                "email" => Auth::id()
+            ]);
+            return $response->ok() && isset($response->json()[0]) ? (object)$response->json()[0] : false;
+        }
+        return false;
+    }
+
+    protected function getProfileById(string $id): object|bool
+    {
+        $token = $this->getToken();
+        if($token) {
+            $response = Http::withHeaders([
+                'Authorization' => 'Bearer '.$token
+            ])->get(env('KEYCLOAK_BASE_URL', '').'/admin/realms/'.env('KEYCLOAK_REALM', 'master').'/users/'.$id);
+
+            return $response->ok() ? (object)$response->json() : false;
+        }
+        return false;
+    }
+
+    protected function getUsers(): array | bool
+    {
+        $token = $this->getToken();
+        if($token) {
+            $response = Http::withHeaders([
+                'Authorization' => 'Bearer '.$token
+            ])->get(env('KEYCLOAK_BASE_URL', '').'/admin/realms/'.env('KEYCLOAK_REALM', 'master').'/users?enabled=true');
+
+            if($response->ok()) {
+                return array_map(function($user) { return (object)$user; }, $response->json());
+            }
+        }
+        return false;
     }
 }
